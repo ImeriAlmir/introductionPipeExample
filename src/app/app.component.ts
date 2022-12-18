9de1d940a4b71b0b7ad8d8165beabfcec623e3b6
Almir Imeri <a.imeri@gentics.com>
2022-12-18 21:50:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'introductionPipeExample';
+    exampleObject: Object = {
+        firstName: 'John',
+        lastName: 'Doe',
+        programmingLanguages: [
+            'Java', 'C', 'C++', 'Typescript'
+        ]
+    };
+    exampleDate: number = Date.now();
+    exampleCurrency = '0.69';
+    exampleDecimal: number = 3.14159265359;
+    exampleString: string = 'Lorem Ipsum';
+    examplePercent: number = 0.697;
+    exampleSlice: string = 'ABCDEFGHIJK';
+    exampleAysnc = new Observable<string>((observer: Observer<string>) => {
+        setInterval(() => observer.next(new Date().toString()), 1000);
+    });
 }
